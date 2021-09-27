c3dd429daa828e0eebc0fea8f6971b41f26f035f
Michael Carr <mcarr2468@gmail.com>
2021-09-27 10:59:53 -0500

    V1.1 html and css done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 
 function App() {
   return (
-    <div className="App">
-      hello world
-    </div>
+    <>
+    <section className="sidebar show-sidebar">
+      <button className="sidebar__close-btn ">&times;</button>
+      <ul className="sidebar__items">
+        <li className="sidebar__item">Home</li>
+        <li className="sidebar__item">About</li>
+        <li className="sidebar__item">Blog</li>
+        <li className="sidebar__item">Documents</li>
+      </ul>
+    
+    </section>
+
+    <button className="open-sidebar-btn">bars here</button>
+
+      <section className="modal-container">
+        <div className="modal">
+          <button className="modal__close-btn">&times;</button>
+          <div className="modal-content">
+            <h1>This is the modal content</h1>
+          </div>
+        </div>
+      </section>
+
+      <button className="open-modal-btn">Show modal content</button>
+    </>
   );
 }
 
